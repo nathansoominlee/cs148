ad78e3c59994ad52342bcfc796c2100711416ea4
Nathan Lee <leeso@stanford.edu>
2015-09-24 11:41:32 -0700

    Project running after XCode configuration to support C++11 and lowering the deployment version to 10.10 from 10.11. Finished.

---
+++ cs148opengl4/source/build/xcode/cs148-opengl4.build/Release/ALL_BUILD.build/Script-5349B472B3A34F2E80F9FB8A.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+make -C /Users/nathanlee/workspace/cs148/cs148opengl4/source/build/xcode -f /Users/nathanlee/workspace/cs148/cs148opengl4/source/build/xcode/CMakeScripts/ALL_BUILD_cmakeRulesBuildPhase.make$CONFIGURATION all
---
+++ cs148opengl4/source/build/xcode/cs148-opengl4.build/Release/cs148opengl4.build/Objects-normal/x86_64/AssignmentN.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/nathanlee/workspace/cs148/cs148opengl4/source/assignment1/AssignmentN.cpp
