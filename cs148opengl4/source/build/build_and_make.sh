9d98cb2436e842fca60a560ac81126d48dcf508e
mmpozulp <mmpozulp@email.wm.edu>
2015-10-03 15:49:03 -0700

    added build script and updated gitignore to ignore build files

---
+++ cs148opengl4/source/build/build_and_make.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# build_and_make.sh
+# Builds the Makefiles then runs make
+
+# Change this for different assignments
+ASSIGNMENT=1
+
+# The build location
+MIKE_DIR=unix/Debug
+NATHAN_DIR=xcode
+
+# Turn on command echoing
+set -x
+
+if [[ `hostname` == "coche" ]]; then
+
+    echo "---> Building for Mike"
+
+    # Make the directory if it doesn't exist
+    mkdir -p $MIKE_DIR
+    
+    cd $MIKE_DIR
+
+    # Build and make
+    cmake ../../../ -G "Unix Makefiles" -DASSIGNMENT=$ASSIGNMENT -DCMAKE_BUILD_TYPE=CONFIG && \
+        make
+
+else
+
+    echo "---> Building for Nathan"
+    mkdir -p $NATHAN_DIR
+    cd $NATHAN_DIR
+    cmake ../../ -G "Xcode" -DASSIGNMENT=$ASSIGNMENT
+
+fi
