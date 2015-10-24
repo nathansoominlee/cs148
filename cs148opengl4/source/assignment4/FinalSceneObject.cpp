d53f4eb8fbcb927058ac2a0cf268a9ff50c44633
mmpozulp <mmpozulp@email.wm.edu>
2015-10-23 23:59:30 -0700

    fixed broken commit

--- cs148opengl4/source/assignment4/FinalSceneObject.cpp
+++ cs148opengl4/source/assignment4/FinalSceneObject.cpp
@@ -16,100 +16,153 @@
 //
 // Which we have have in rows and need to turn into a container of FinalSceneObjects
 
+// Driver function for ParseSFO
 int FinalSceneObject::MakeContainer(const Rows &rows,                      // input parameter
                       std::vector<FinalSceneObject> &final_scene_objects)  // output parameter
 {
-    std::string name;
-    std::string description;
-    std::string texture_path;
-    FinalSceneObject::Shader shader;
-    float epic_metallic;
-    float epic_roughness;
-    float epic_specular;
-    glm::vec4 bp_diffuse;
-    glm::vec4 bp_specular;
-    float scale;
-    float tx;
-    float ty;
-    float tz;
-    float rx;
-    float ry;
-    float rz;
-
-    std::string field;
-    for (auto row : rows)
+    // r = 1 skips the first row of headers
+    for (int r = 1; r < rows.size(); r++)
     {
+        auto row = rows[r];
 
-        for (int i = 0; i < row.size(); i++)
+        if (row.size() > 0 && row[0] == "y")
         {
+            //std::cout << "Told to parse: " << std::endl;
+            //PrintRow(row);
 
-            field = row[i];
-
-            switch(i)
-            {
-
-                case(TryToParse):
-
-                    if (field == 'n')
-                    {
-                        continue;
-                    }
-
-                    break;
-
-                case(Object):
-
-                    name = field;
-                    break;
-
-                case(Description):
-
-                    description = field;
-                    break;
-
-                case(Texture):
-
-                    texture_path = field;
-                    break;
-
-                case(Shader):
-
-                    FinalSceneObject::ParseShader(&field,
-                                                  &shader,
-                                                  &epic_metallic,
-                                                  &epic_roughness,
-                                                  &epic_specular,
-                                                  &bp_diffuse,
-                                                  &bp_specular);
-                    break;
-
-                case(Transformations)
-                    // TODO: Parse transformations
-                    break;
-                case(Rotations)
-                    // TODO: Parse rotations 
-                    break;
-            }
+            FinalSceneObject fso = ParseFSO(row);
+            final_scene_objects.push_back(fso);
         }
-        std::cout << std::endl;
+        else if (row.size() > 0 && row[0] == "n")
+        {
+            // We are told to skip this object
+        }
+        else
+        {
+            std::cerr << "Error: Unsure whether to parse row based on indicator: " << std::endl;
+            PrintRow(row);
+            exit(1);
+        }
+
+    } // for j
+
+    for (auto object : final_scene_objects)
+    {
+        std::cout << object << std::endl;
     }
+
+} // FinalSceneObject::MakeContainer()
+
+void FinalSceneObject::PrintRow(std::vector<std::string> row)
+{
+    for (auto field : row)
+    {
+        std::cout << field << " ";
+    }
+
+    std::cout << std::endl;
 }
 
+FinalSceneObject FinalSceneObject::ParseFSO(std::vector<std::string> row)
+{
+    std::string name = "";
+    std::string description = "";
+    std::string texture_path = "";
+    ShaderType shader = ShaderType::None;
+    float epic_metallic = -1.f;
+    float epic_roughness = -1.f;
+    float epic_specular = -1.f;
+    glm::vec4 bp_diffuse = glm::vec4(-1.f, -1.f, -1.f, -1.f);
+    glm::vec4 bp_specular = glm::vec4(-1.f, -1.f, -1.f, -1.f);
+    float scale = -1.f;
+    float tx = -1.f;
+    float ty = -1.f;
+    float tz = -1.f;
+    float rx = -1.f;
+    float ry = -1.f;
+    float rz = -1.f;
+
+
+    std::string field;
+    for (int i = 0; i < row.size(); i++)
+    {
+        field = row[i];
+
+        switch(i)
+        {
+
+            case(Column::TryToParse):
+
+                // already handled this column
+                break;
+
+            case(Column::Object):
+
+                name = field;
+                std::cout << "Reading in " << name << ": ";
+                break;
+
+            case(Column::Description):
+
+                description = field;
+                std::cout << description << std::endl;
+                break;
+
+            case(Column::Texture):
+
+                texture_path = field;
+                break;
+
+            case(Column::Shader):
+
+                FinalSceneObject::ParseShader(field,
+                                              shader,
+                                              &epic_metallic,
+                                              &epic_roughness,
+                                              &epic_specular,
+                                              bp_diffuse,
+                                              bp_specular);
+                break;
+
+            case(Column::Scale):
+                FinalSceneObject::ParseScale(field, &scale);
+                break;
+            case(Column::Transformations):
+                FinalSceneObject::ParseTransformations(field, &tx, &ty, &tz);
+                break;
+            case(Column::Rotations):
+                FinalSceneObject::ParseRotations(field, &rx, &ry, &rz);
+                break;
+        } // switch
+
+    } // for i
+
+    // Build and return the object
+    FinalSceneObject fso(name, description, texture_path, shader, \
+                    epic_metallic, epic_roughness, epic_specular, bp_diffuse, bp_specular, \
+                    scale, tx, ty, tz, rx, ry, rz);
+
+    return fso;
+
+} //FinalSceneObject::ParseFSO()
+
 // returns 0 on success
-int FinalSeneObject::ParseShader(const std::string& field,          // input parameter
-                                 FinalSceneObject::Shader& shader,  // output parameter
-                                 float& epic_metallic,              // output parameter
-                                 float& epic_roughness,             // output parameter
-                                 float& epic_specular,              // output parameter
-                                 float& bp_diffuse,                 // output parameter
-                                 float& bp_specular)                // output parameter
+int FinalSceneObject::ParseShader(const std::string& field,          // input parameter
+                                 ShaderType& shader,  // output parameter
+                                 float* epic_metallic,              // output parameter
+                                 float* epic_roughness,             // output parameter
+                                 float* epic_specular,              // output parameter
+                                 glm::vec4& bp_diffuse,                 // output parameter
+                                 glm::vec4& bp_specular)                // output parameter
 {
     // Should look like either 
-    // Epic:0.5,0,0.6 or BP:(0.1,0.2,0.3),(0.7, 0.2, 0.9)
+    // Epic:0.5,0,0.6 or BP:0.5,0.5,0.5,1.0,0.5,0.5,0.5,1.0
 
     const char *haystack = field.c_str();
     const char *colon = ":"; 
     char *colon_location;
+
+    //std::cout << "ParseShader, field: " << field << std::endl;
 
     colon_location = strnstr(haystack, colon, field.length());
 
@@ -129,9 +182,9 @@ int FinalSeneObject::ParseShader(const std::string& field,          // input par
     if (shader_spec == "Epic")
     {
         // Set what we know
-        shader = FinalSceneObject::Shader::Epic;
-        bp_diffuse = -1;
-        bp_specular = -1;
+        shader = ShaderType::Epic;
+        bp_diffuse = glm::vec4(-1.f, -1.f, -1.f, -1.f);
+        bp_specular = glm::vec4(-1.f, -1.f, -1.f, -1.f);
 
         // Process Epic parameters
         haystack = colon_location + 1;
@@ -145,10 +198,10 @@ int FinalSeneObject::ParseShader(const std::string& field,          // input par
     else if (shader_spec == "BP")
     {
         // Set what we know
-        shader = FinalSceneObject::Shader::BP;
-        epic_metallic = -1;
-        epic_roughness = -1;
-        epic_specular = -1;
+        shader = ShaderType::BP;
+        (*epic_metallic) = -1;
+        (*epic_roughness) = -1;
+        (*epic_specular) = -1;
 
         haystack = colon_location + 1;
         std::string bp_params(haystack);
@@ -159,7 +212,7 @@ int FinalSeneObject::ParseShader(const std::string& field,          // input par
     }
     else
     {
-        std::cerr << "Error: Unrecognized shader specificatoin " << shader_spec << std::endl;
+        std::cerr << "Error: Unrecognized shader specification " << shader_spec << std::endl;
         exit(1);
     }
 
@@ -168,30 +221,31 @@ int FinalSeneObject::ParseShader(const std::string& field,          // input par
 
 // returns 0 on success
 int FinalSceneObject::ParseEpicParams(const std::string& epic_params, // input parameter
-                                      float& epic_metallic,           // output parameter
-                                      float& epic_roughness,          // output parameter
-                                      float& epic_specular)           // output parameter
+                                      float* epic_metallic,           // output parameter
+                                      float* epic_roughness,          // output parameter
+                                      float* epic_specular)           // output parameter
 {
 
     // Collect the 3 parameters
 
     std::string::size_type sz = 0;
-    for (int j = 0; j < 3; j++)
+    for (int i = 0; i < 3; i++)
     {
 
         float param = std::stof(epic_params.substr(sz), &sz);
+        sz += 1;
 
-        if (j == 0) 
+        if (i == 0) 
         {
-            epic_metallic = param;
+            (*epic_metallic) = param;
         }
-        else if (j == 1)
+        else if (i == 1)
         {
-            epic_roughness = param;
+            (*epic_roughness) = param;
         }
-        else //if (j == 2)
+        else //if (i == 2)
         {
-            epic_specular = param;
+            (*epic_specular) = param;
         }
 
     } 
@@ -199,9 +253,165 @@ int FinalSceneObject::ParseEpicParams(const std::string& epic_params, // input p
     return 0;
 }
 
+// returns 0 on success
 int FinalSceneObject::ParseBPParams(const std::string& bp_params, // input parameter
                                     glm::vec4 &bp_diffuse,        // output parameter
-                                    glm::vec4 &epic_roughness);   // output parameter
+                                    glm::vec4 &bp_specular)    // output parameter
 {
-    // TODO: Collect the 2 parameter vectors
+    // Collect the 4 parameters for each vec4, 4 * 2 = 8 
+    int n = 8;
+    
+    std::string::size_type sz = 0;
+    float v1 = -1;
+    float v2 = -1;
+    float v3 = -1;
+    float v4 = -1;
+    for (int i = 0; i < n; i++)
+    {
+        float param = std::stof(bp_params.substr(sz), &sz);
+        sz += 1;
+
+        if ( i == 0 || (i == n - 4) )
+        {
+            v1 = param;
+        }
+        else if ( i == 1 || (i == n - 3) )
+        {
+            v2 = param;
+        }
+        else if ( i == 2 || (i == n - 2) )
+        {
+            v3 = param;
+        }
+        else if ( i == 3 || (i == n - 1) )
+        {
+            v4 = param;
+            if (i == 3)
+            {
+                bp_diffuse = glm::vec4(v1, v2, v3, v4);
+                v1 = -1;
+                v2 = -1;
+                v3 = -1;
+                v4 = -1;
+            }
+            else if (i == n - 1)
+            {
+                bp_specular = glm::vec4(v1, v2, v3, v4);
+            }
+        }
+    }
+
+    return 0;
+}
+
+// Returns 0 on success
+int FinalSceneObject::ParseTransformations(const std::string& t_params,
+                                           float* tx, float* ty, float* tz)
+{
+    std::string::size_type sz = 0;
+    for (int i = 0; i < 3; i++)
+    {
+        float param = std::stof(t_params.substr(sz), &sz);
+        sz += 1;
+
+        if (i == 0)
+        {
+            (*tx) = param;
+        }
+        else if (i == 1)
+        {
+            (*ty) = param;
+        }
+        else //if (i == 1)
+        {
+            (*tz) = param;
+        }
+    }
+
+    return 0;
+}
+
+// Returns 0 on success
+int FinalSceneObject::ParseRotations(const std::string& r_params,
+                                     float* rx, float* ry, float* rz)
+{
+    std::string::size_type sz = 0;
+    for (int i = 0; i < 3; i++)
+    {
+        float param = std::stof(r_params.substr(sz), &sz);
+        sz += 1;
+
+        if (i == 0)
+        {
+            (*rx) = param;
+        }
+        else if (i == 1)
+        {
+            (*ry) = param;
+        }
+        else //if (i == 1)
+        {
+            (*rz) = param;
+        }
+    }
+    return 0;
+}
+
+// Returns 0 on success
+int FinalSceneObject::ParseScale(const std::string& scale_param,
+                                 float* scale)
+{
+    float param = std::stof(scale_param);
+    (*scale) = param;
+    return 0;
+}
+
+void FinalSceneObject::PrintContainer(const std::vector<FinalSceneObject>& final_scene_objects)
+{
+    std::cout << "container.size(): " << final_scene_objects.size() << std::endl;
+    for (int i = 0; i < final_scene_objects.size(); i++)
+    {
+        std::cout << "i: " << i << " fso: " << final_scene_objects[i] << std::endl;
+    }
+}
+
+std::ostream& operator<< (std::ostream& os, const FinalSceneObject& fso)
+{
+    std::string shader = "None";
+    switch(fso.shader)
+    {
+        case (FinalSceneObject::ShaderType::Epic):
+
+            shader = "Epic";
+            break;
+
+        case (FinalSceneObject::ShaderType::BP):
+
+            shader = "BP";
+            break;
+
+        case (FinalSceneObject::ShaderType::None):
+
+            shader = "None";
+            break;
+    }
+
+    os << "name: "           << fso.name
+       << " description: "    << fso.description
+       << " texture_path: "   << fso.texture_path
+       << " shader: "         << shader
+       << " epic_metallic: "  << fso.epic_metallic
+       << " epic_roughness: " << fso.epic_roughness
+       << " epic_specular: "  << fso.epic_specular
+       << " bp_diffuse: "     << fso.bp_diffuse[0] << " " << fso.bp_diffuse[1] << " " << fso.bp_diffuse[2] << " " << fso.bp_diffuse[3]
+       << " bp_specular: "    << fso.bp_specular[0] << " " << fso.bp_specular[1] << " " << fso.bp_specular[2] << " " << fso.bp_specular[3]
+       << " scale: "          << fso.scale
+       << " tx: "             << fso.tx
+       << " ty: "             << fso.ty
+       << " tz: "             << fso.tz
+       << " rx: "             << fso.rx
+       << " ry: "             << fso.ry
+       << " rz: "             << fso.rz;
+
+    return os;
 }
--- cs148opengl4/source/assignment4/FinalSceneObject.h
+++ cs148opengl4/source/assignment4/FinalSceneObject.h
@@ -13,28 +13,32 @@ public:
 
     // Factory function to create a collection from the google sheet
     static int MakeContainer(const Rows &rows,                              // input parameter
-                      std::vector<FinalSceneObject> &final_scene_objects);  // output parameter
+                      std::vector<FinalSceneObject>& final_scene_objects);  // output parameter
+
+    // Print function for the created container
+    static void PrintContainer(const std::vector<FinalSceneObject>& final_scene_objects);
 
 private:
 
 // The column data looks like this
 // Try to parse? object description texture shader:p1,p2{,p3} transformations:tx,ty,tz 
 
-    enum class Column {TryToParse      = 0,
-                       Object          = 1,
-                       Description     = 2,
-                       Texture         = 3,
-                       Shader          = 4,
-                       Transformations = 5,
-                       Rotations       = 6
-                      };
+    enum Column {TryToParse      = 0,
+                 Object          = 1,
+                 Description     = 2,
+                 Texture         = 3,
+                 Shader          = 4,
+                 Scale           = 5,
+                 Transformations = 6,
+                 Rotations       = 7
+                };
 
-    enum class Shader { Epic, BP };
+    enum class ShaderType { None, Epic, BP };
 
     FinalSceneObject(std::string name, 
                      std::string description, 
                      std::string texture_path,
-                     FinalSceneObject::Shader shader,
+                     ShaderType shader,
                      float epic_metallic,
                      float epic_roughness,
                      float epic_specular,
@@ -65,29 +69,42 @@ private:
         rz(rz)
     {}
 
+    static void PrintRow(std::vector<std::string> row);
+
+    static FinalSceneObject ParseFSO(std::vector<std::string> row);
 
     static int ParseShader(const std::string& field,          // input parameter
-                           FinalSceneObject::Shader& shader,  // output parameter
-                           float& epic_metallic,              // output parameter
-                           float& epic_roughness,             // output parameter
-                           float& epic_specular,              // output parameter
-                           float& bp_diffuse,                 // output parameter
-                           float& bp_specular);               // output parameter
+                           ShaderType& shader,  // output parameter
+                           float* epic_metallic,              // output parameter
+                           float* epic_roughness,             // output parameter
+                           float* epic_specular,              // output parameter
+                           glm::vec4& bp_diffuse,                 // output parameter
+                           glm::vec4& bp_specular);               // output parameter
 
     static int ParseEpicParams(const std::string& epic_params, // input parameter
-                               float& epic_metallic,           // output parameter
-                               float& epic_roughness,          // output parameter
-                               float& epic_specular);          // output parameter
+                               float* epic_metallic,           // output parameter
+                               float* epic_roughness,          // output parameter
+                               float* epic_specular);          // output parameter
 
     static int ParseBPParams(const std::string& bp_params, // input parameter
                              glm::vec4 &bp_diffuse,        // output parameter
-                             glm::vec4 &epic_roughness);   // output parameter
+                             glm::vec4 &bp_specular);   // output parameter
 
+    static int ParseTransformations(const std::string& t_params,
+                                    float* tx, float* ty, float* tz);
+
+    static int ParseRotations(const std::string& r_params,
+                              float* rx, float* ry, float* rz);
+
+    static int ParseScale(const std::string& scale_param,
+                          float* scale);
+
+    friend std::ostream& operator<< (std::ostream& os, const FinalSceneObject& fso);
 
     std::string name;
     std::string description;
     std::string texture_path;
-    FinalSceneObject::Shader shader;
+    ShaderType shader;
     float epic_metallic;
     float epic_roughness;
     float epic_specular;
